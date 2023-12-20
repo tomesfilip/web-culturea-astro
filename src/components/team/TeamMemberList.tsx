aaeebdb30c891abd569ac38fb6f5d4dd16c1b6a6
WilloS88 <ramon2003@email.cz>
2023-12-20 22:25:33 +0100

    resolved destructuring

--- src/components/team/TeamMemberList.tsx
+++ src/components/team/TeamMemberList.tsx
@@ -8,6 +8,7 @@ import '../../styles/swiper.css'
 
 import type { TTeamMemberItem } from '../../lib/types/TTeamMemberItem'
 
+
 interface Props {
   teamMembers: TTeamMemberItem[]
 }
@@ -32,17 +33,17 @@ const TeamMemberList = ({ teamMembers }: Props) => {
         },
       }}
     >
-      {teamMembers.map((teamMember, index) => (
-        <SwiperSlide key={index}>
+      {teamMembers.map(({name, img, position, email, phone}, index) => (
+        <SwiperSlide key={`${name}-${index}`}>
           <figure className="flex flex-col justify-center mx-2 2xl:mx-8 text-center rounded-lg">
             <div>
             <img
               className="object-cover object-center rounded-t-lg opacity-70 hover:opacity-100 transition-opacity duration-300 "
-              src={teamMember.img}
+              src={img}
               width={400}
               height={500}
               loading="lazy"
-              alt={teamMember.name}
+              alt={name}
             />
             
             <figcaption
@@ -50,22 +51,22 @@ const TeamMemberList = ({ teamMembers }: Props) => {
               style={{ backgroundColor: index % 2 === 0 ? '#DE5C0F' : '#19777A' }}
             >
               <span className="mb-2 name text-bold text-2xl uppercase">
-                {teamMember.name}
+                {name}
               </span>
               <h4 className="mb-0 uppercase font-normal">
-                {teamMember.position}
+                {position}
               </h4>
               <a
                 className="block uppercase font-normal"
-                href={`mailto:${teamMember.email}`}
+                href={`mailto:${email}`}
               >
-                {teamMember.email}
+                {email}
               </a>
               <a
                 className="block uppercase font-normal"
-                href={`tel:${teamMember.phone}`}
+                href={`tel:${phone}`}
               >
-                {teamMember.phone}
+                {phone}
               </a>
             </figcaption>
             </div>
