c523d41fdf339d7262a1e03be82c45f44b756b66
WilloS88 <ramon2003@email.cz>
2023-12-20 22:24:55 +0100

    resolved constant for the year

--- src/components/header/LogoLink.tsx
+++ src/components/header/LogoLink.tsx
@@ -1,10 +1,14 @@
+const getCurrentYear = (): number => {
+  return new Date().getFullYear();
+};
+
 const LogoLink = () => {
   return (
     <a className="logo" href="/">
-      <h1 className="hidden">Culturea 2024</h1>
+      <h1 className="hidden">Culturea {getCurrentYear()}</h1>
       <img
-        src="../../../public/logo_slovo.svg"
-        alt="culturea 2024 logo"
+        src="./logo_slovo.svg"
+        alt={`culturea ${getCurrentYear()} logo`}
         width={200}
         height={46}
       />
