7d3990079bf92a2b485bfd8625be83ba3223f946
WilloS88 <ramon2003@email.cz>
2023-12-20 22:22:17 +0100

    resolved cookie

--- src/components/cookies/CookieHeader.tsx
+++ src/components/cookies/CookieHeader.tsx
@@ -1,14 +1,12 @@
-import cookieIcon from '../../assets/img/icons/cookie.svg';
+import cookieIcon from '../../assets/img/icons/cookie.svg?url'
 
 const CookieHeader = () => {
   return (
     <div className="cookie-header flex flex-wrap items-center gap-x-4">
-      {/* Use Tailwind's utility classes for background images */}
-      <div className="bg-contain bg-center bg-no-repeat w-12 h-12" style={{ backgroundImage: `url(${cookieIcon})` }} aria-label="cookie icon"></div>
+      <img src={cookieIcon} alt="cookie ikona" width={50} height={50} />
       <h4 className="text-white md:text-2xl text-xl font-bold">Cookies</h4>
     </div>
-  );
-};
+  )
+}
 
-export default CookieHeader;
-
+export default CookieHeader
