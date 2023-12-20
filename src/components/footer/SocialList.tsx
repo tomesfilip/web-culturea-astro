fa1db803aa87abe1b1ada07f897ba05b9113562c
WilloS88 <ramon2003@email.cz>
2023-12-20 22:24:28 +0100

    resolved styling and implemented addition styling

--- src/components/footer/SocialList.tsx
+++ src/components/footer/SocialList.tsx
@@ -5,7 +5,7 @@ import SocialItem from './SocialItem';
 const SocialList = () => {
   return (
     <div className="socials-container">
-      <div className="flex flex-col gap-6 opacity-50">
+      <div className="space-y-6 opacity-50">
         {socialItems.map(({ imgSrc, imgAlt, socLink }) => (
           <SocialItem
             key={socLink}
