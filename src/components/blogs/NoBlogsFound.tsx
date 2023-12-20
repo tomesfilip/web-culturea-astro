3a56de572dce4cc17adc34e0a8f7f975f21af3fe
Filip Tomes <filiptomes@protonmail.com>
2023-12-21 00:52:12 +0100

    removed blogs functionality

--- src/components/blogs/NoBlogsFound.tsx
+++
@@ -1,9 +0,0 @@
-const NoBlogsFound = () => {
-  return (
-    <div className="no-blogs ">
-      <p className="text-xl font-bold">Zatím nebyly přidány žádné blogy.</p>
-    </div>
-  );
-};
-
-export default NoBlogsFound;
