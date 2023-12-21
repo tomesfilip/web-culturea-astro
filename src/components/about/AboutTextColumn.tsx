c711e61ac65ac32f86f47b5b214781f59ad3f08f
Filip Tomes <filiptomes@protonmail.com>
2023-12-21 19:43:01 +0100

    style: responsive about section, fixed landing issues

---
+++ src/components/about/AboutTextColumn.tsx
@@ -0,0 +1,25 @@
+export const AboutTextColumn = () => {
+  return (
+    <div className="space-y-8">
+      <p className="text-xl lg:text-2xl">
+        Festival přinášející do Zlína esenci dalekých krajin
+      </p>
+      <div className="text-lg lg:text-xl w-full space-y-4">
+        <p>
+          Culturea je festival, který umožňuje poznání cizích kultur všemi
+          smysly. Nabízí návštěvníkům možnost vidět, slyšet, ochutnat, dotknout
+          se a pocítit atmosféru dalekých krajin na vlastní kůži.
+        </p>
+        <p>
+          Každý ročník festival Culturea představí dvě země, které jsou od sebe
+          poměrně vzdálené a na první pohled zcela odlišné, ve skrytu jsou si
+          však něčím velmi podobné.
+        </p>
+        <p>
+          Již dvanáctý rok projekt organizují studenti Fakulty multimediálních
+          komunikací Univerzity Tomáše Bati ve Zlíně.
+        </p>
+      </div>
+    </div>
+  );
+};
--- src/sections/SectionAbout.tsx
+++ src/sections/SectionAbout.tsx
@@ -1,43 +1,24 @@
+import { AboutTextColumn } from '../components/about/AboutTextColumn';
 import { Section } from '../components/shared/Section';
 
 export const SectionAbout = () => {
   return (
     <Section sectionId="o-festivalu">
-      <div className="flex text-white mx-40 mt-20 pb-40">
-        <div className="w-1/2 flex flex-col pb-20 justify-center">
-          <h2 className="font-bold text-left ">O festivalu</h2>
-          <p className="mb-8 text-left text-2xl">
-            Festival přinášející do Zlína esenci dalekých krajin
-          </p>
-          <div className="flex flex-col justify-end items-end text-xl px-20 pr-40 text-left w-full">
-            <p className="mb-4">
-              Culturea je festival, který umožňuje poznání cizích kultur všemi
-              smysly. Nabízí návštěvníkům možnost vidět, slyšet, ochutnat,
-              dotknout se a pocítit atmosféru dalekých krajin na vlastní kůži.
-            </p>
-            <p className="mb-4">
-              Každý ročník festival Culturea představí dvě země, které jsou od
-              sebe poměrně vzdálené a na první pohled zcela odlišné, ve skrytu
-              jsou si však něčím velmi podobné.
-            </p>
-            <p className="mb-4">
-              Již dvanáctý rok projekt organizují studenti Fakulty
-              multimediálních komunikací Univerzity Tomáše Bati ve Zlíně.
-            </p>
-          </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 justify-items-center text-white pb-40 px-4 lg:px-8 lg:gap-x-16 xl:gap-x-24 gap-y-12">
+        <div className="w-full max-w-[500px] xl:max-w-[540px] order-2 lg:order-1">
+          <h2 className="font-bold">O festivalu</h2>
+          <AboutTextColumn />
         </div>
-        <div className="lg:w-1/2 flex flex-col items-start justify-start relative">
-          <div className="flex items-end justify-end">
-            <img
-              src="/o-festivalu/x-challenge.jpg"
-              alt="X Challenge"
-              className="h-auto w-3/4"
-            />
-          </div>
+        <div className="relative order-1 lg:order-2 grid">
+          <img
+            src="/o-festivalu/x-challenge.jpg"
+            alt="X Challenge"
+            className="h-auto w-11/12 max-w-[500px] justify-self-start lg:justify-self-end"
+          />
           <img
             src="/o-festivalu/tym.jpg"
             alt="O festivalu - tym"
-            className="absolute bottom-4 mt-8 w-1/2"
+            className="w-4/5 max-w-[300px] -mt-20 justify-self-end lg:justify-self-start"
           />
         </div>
       </div>
