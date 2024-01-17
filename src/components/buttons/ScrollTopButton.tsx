265506909329b72447795f9f33b7b406d7400cce
WilloS88 <ramon2003@email.cz>
2024-01-17 10:38:26 +0100

    button fix + 15. task + hromkova phone fix

--- src/components/buttons/ScrollTopButton.tsx
+++ src/components/buttons/ScrollTopButton.tsx
@@ -8,33 +8,22 @@ const ScrollToTopButton = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
-  const baseStyle = {
-    borderColor: 'orange',
-    transition: 'background-color 0.2s ease',
-  };
-  
-  const hoverStyle = {
-    ...baseStyle,
-    backgroundColor: 'orange', 
-  };
-
   return (
     <div>
       <button
         onClick={scrollToTop}
         onMouseEnter={() => setIsHovering(true)}
         onMouseLeave={() => setIsHovering(false)}
-        style={isHovering ? hoverStyle : baseStyle}
-        className="fixed bottom-2 right-2 m-3 p-4 rounded-full border-2"
-      >
-        {isHovering ? (
-          <img src={arrowTopHover} alt="Scroll to top" width={20} height={30} />
-        ) : (
-          <img src={arrowTop} alt="Scroll to top" width={20} height={30} />
-        )}
-      </button>
-    </div>
-  );
-};
-
-export default ScrollToTopButton;
+        className="fixed bottom-2 right-2 m-3 p-4 rounded-full border-2 hover:bg-orange-1 border-orange-1 duration-300"
+        >
+          {isHovering ? (
+            <img src={arrowTopHover} alt="Scroll to top" width={20} height={30} />
+          ) : (
+            <img src={arrowTop} alt="Scroll to top" width={20} height={30} />
+          )}
+        </button>
+      </div>
+    );
+  };
+  
+  export default ScrollToTopButton;
