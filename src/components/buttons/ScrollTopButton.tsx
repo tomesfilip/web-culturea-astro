1d6bd6a5673e99cc24984dcc10c2b2908db510d1
WilloS88 <ramon2003@email.cz>
2024-01-17 20:02:15 +0100

    img to svg

--- src/components/buttons/ScrollTopButton.tsx
+++ src/components/buttons/ScrollTopButton.tsx
@@ -14,16 +14,54 @@ const ScrollToTopButton = () => {
         onClick={scrollToTop}
         onMouseEnter={() => setIsHovering(true)}
         onMouseLeave={() => setIsHovering(false)}
-        className="fixed bottom-2 right-2 m-3 p-4 rounded-full border-2 hover:bg-orange-1 border-orange-1 duration-300"
-        >
-          {isHovering ? (
-            <img src={arrowTopHover} alt="Scroll to top" width={20} height={30} />
-          ) : (
-            <img src={arrowTop} alt="Scroll to top" width={20} height={30} />
-          )}
-        </button>
-      </div>
-    );
-  };
-  
-  export default ScrollToTopButton;
+        className="fixed bottom-2 right-2 m-3 p-4 rounded-full border-4 hover:bg-orange-1 border-orange-1 duration-300"
+      >
+        {isHovering ? (
+          <svg
+            width="50"
+            height="50"
+            viewBox="0 0 235 131"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M10 121L121 9.99999"
+              stroke="white"
+              stroke-width="20"
+              stroke-linecap="round"
+            />
+            <path
+              d="M224.071 121L121 10"
+              stroke="white"
+              stroke-width="20"
+              stroke-linecap="round"
+            />
+          </svg>
+        ) : (
+          <svg
+            width="50"
+            height="50"
+            viewBox="0 0 235 131"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M10 121L121 9.99999"
+              stroke="#FF8A00"
+              stroke-width="20"
+              stroke-linecap="round"
+            />
+            <path
+              d="M224.071 121L121 10"
+              stroke="#FF8A00"
+              stroke-width="20"
+              stroke-linecap="round"
+            />
+          </svg>
+        )}
+      </button>
+    </div>
+  );
+};
+
+export default ScrollToTopButton;
