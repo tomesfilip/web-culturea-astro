b57facda03d71ef96d9c9865e1bc14ef144a5a4f
Filip Tomes <filiptomes@protonmail.com>
2024-01-17 20:43:07 +0100

    replaced button with anchor tag

--- src/components/buttons/ScrollTopButton.tsx
+++ src/components/buttons/ScrollTopButton.tsx
@@ -1,67 +1,20 @@
-import arrowTop from '../../../public/ikony/scroll-top.svg?url';
-import arrowTopHover from '../../../public/ikony/scroll-top-hover.svg?url';
-import { useState } from 'react';
-
-const ScrollToTopButton = () => {
-  const [isHovering, setIsHovering] = useState(false);
-  const scrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  };
-
+export const ScrollToTopButton = () => {
   return (
-    <div>
-      <button
-        onClick={scrollToTop}
-        onMouseEnter={() => setIsHovering(true)}
-        onMouseLeave={() => setIsHovering(false)}
-        className="fixed bottom-2 right-2 m-3 p-4 rounded-full border-4 hover:bg-orange-1 border-orange-1 duration-300"
+    <a
+      href="#"
+      className="fixed bottom-2 right-2 m-3 p-4 rounded-full border-4 hover:bg-orange-1 border-orange-1 duration-300 group"
+    >
+      <svg
+        className="stroke-orange-1 group-hover:stroke-white"
+        width="50"
+        height="50"
+        viewBox="0 0 235 131"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
       >
-        {isHovering ? (
-          <svg
-            width="50"
-            height="50"
-            viewBox="0 0 235 131"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M10 121L121 9.99999"
-              stroke="white"
-              stroke-width="20"
-              stroke-linecap="round"
-            />
-            <path
-              d="M224.071 121L121 10"
-              stroke="white"
-              stroke-width="20"
-              stroke-linecap="round"
-            />
-          </svg>
-        ) : (
-          <svg
-            width="50"
-            height="50"
-            viewBox="0 0 235 131"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M10 121L121 9.99999"
-              stroke="#FF8A00"
-              stroke-width="20"
-              stroke-linecap="round"
-            />
-            <path
-              d="M224.071 121L121 10"
-              stroke="#FF8A00"
-              stroke-width="20"
-              stroke-linecap="round"
-            />
-          </svg>
-        )}
-      </button>
-    </div>
+        <path d="M10 121L121 9.99999" strokeWidth="20" strokeLinecap="round" />
+        <path d="M224.071 121L121 10" strokeWidth="20" strokeLinecap="round" />
+      </svg>
+    </a>
   );
 };
-
-export default ScrollToTopButton;
