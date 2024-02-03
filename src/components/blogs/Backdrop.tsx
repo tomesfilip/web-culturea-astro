79a48f6bca2895459df6a13401360afca98a6032
Filip Tomes <filiptomes@protonmail.com>
2024-02-03 13:10:50 +0100

    backdrop for a modal

---
+++ src/components/blogs/Backdrop.tsx
@@ -0,0 +1,21 @@
+import { motion } from 'framer-motion';
+import type { ReactNode } from 'react';
+
+type Props = {
+  onClick: () => void;
+  children: ReactNode;
+};
+
+export const Backdrop = ({ onClick, children }: Props) => {
+  return (
+    <motion.div
+      className="absolute top-0 left-0 h-full w-full bg-black bg-opacity-70 flex justify-center items-center"
+      onClick={onClick}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
+      {children}
+    </motion.div>
+  );
+};
