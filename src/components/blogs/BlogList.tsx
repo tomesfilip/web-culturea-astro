166b5f607ea89aa14b06a9c9afcb438595fc868e
Filip Tomes <filiptomes@protonmail.com>
2024-02-03 14:25:52 +0100

    blog list and blog list item components

---
+++ src/components/blogs/BlogList.tsx
@@ -0,0 +1,24 @@
+import { useFetchBlogList } from '../../hooks/useFetchBlogList';
+import type { TBlogItem } from '../../types/TBlogItem';
+import { Loader } from '../shared/Loader';
+import { BlogListItem } from './BlogListItem';
+
+export const BlogList = () => {
+  const { data: blogs, isLoading, error } = useFetchBlogList();
+
+  return (
+    <>
+      {isLoading && <Loader />}
+      {!isLoading && (!blogs || blogs.length < 1) && (
+        <p className="text-xl font-bold">Zatím nebyly přidány žádné blogy.</p>
+      )}
+      {blogs.length > 0 && (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8">
+          {blogs.map((blog: TBlogItem) => (
+            <BlogListItem key={blog.id} blog={blog} />
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
---
+++ src/types/TBlogItem.ts
@@ -0,0 +1,10 @@
+export type TBlogItem = {
+  id: string;
+  author: string;
+  bannerImage: string;
+  body: string;
+  brief: string;
+  category: string;
+  createdAt: string;
+  title: string;
+};
