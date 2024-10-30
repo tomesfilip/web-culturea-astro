776cb3129843371c15f0d8e6eac9f2f8c69ba4e8
Filip Tomes <filiptomes@protonmail.com>
2024-10-30 23:02:32 +0100

    video renamed

--- src/components/history/ThisYearVideo.tsx
+++ src/components/history/ThisYearVideo.tsx
@@ -1,10 +1,8 @@
-import videoSrc from '../../assets/videos/Culturea-Aftermovie-2024.mp4';
-
 export const ThisYearVideo = () => {
   return (
     <div className="w-full flex justify-center mt-10">
       <video width="560" height="315" controls>
-        <source src={videoSrc} type="video/mp4" />
+        <source src="/culturea-aftermovie-2024.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
