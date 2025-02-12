3edf17e5294201581231d88b42a6695723168bf1
Filip Tomes <filiptomes@protonmail.com>
2025-02-12 22:07:12 +0100

    logout fix

--- src/utils/logout.ts
+++ src/utils/logout.ts
@@ -2,6 +2,9 @@ import { signOut } from 'firebase/auth';
 import { auth } from '../config/firebase';
 
 export const logout = async () => {
+  if (!auth) {
+    return;
+  }
   try {
     await signOut(auth);
   } catch (error) {
