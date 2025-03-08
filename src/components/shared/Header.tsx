b64698a13764a36a0842ece3a15bfbc33355251a
Pavel Krůček <94629483+Pavos21@users.noreply.github.com>
2025-03-08 15:48:38 +0100

    Pre-sale update (#49)

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,11 +1,11 @@
-import { AnimatePresence, motion } from 'framer-motion';
-import { useState } from 'react';
-import { Twirl as Hamburger } from './hamburger';
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
+import { Twirl as Hamburger } from "./hamburger";
 
-import { menuItems } from '../../data/menuItems';
-import { LogoLink } from '../header/LogoLink';
-import { MenuLink } from '../header/MenuLink';
-import { TicketButton } from '../header/TicketButton';
+import { menuItems } from "../../data/menuItems";
+import { LogoLink } from "../header/LogoLink";
+import { MenuLink } from "../header/MenuLink";
+import { TicketButton } from "../header/TicketButton";
 
 interface Props {
   path?: string;
@@ -28,18 +28,21 @@ export const Header = ({ path }: Props) => {
   return (
     <motion.header className="w-full flex justify-center bg-dark-green fixed top-0 z-20">
       <nav className="relative z-10 max-w-screen-2xl w-full flex flex-col items-center xl:flex-row xl:justify-between xl:py-4 xl:px-12">
+        {/* Desktop Logo */}
         <div className="hidden xl:block pr-8">
           <LogoLink />
         </div>
+
+        {/* Mobile Layout: Logo, TicketButton, and Hamburger */}
         <div className="z-20 flex w-full justify-between items-center p-4 xl:hidden">
           <LogoLink />
-          <Hamburger
-            rounded
-            toggled={isOpen}
-            toggle={setIsOpen}
-            color="beige"
-          />
+          <div className="ml-auto pl-2 mr-2">
+            <TicketButton />
+          </div>
+          <Hamburger rounded toggled={isOpen} toggle={setIsOpen} color="beige" />
         </div>
+
+        {/* Mobile Menu */}
         {isOpen && (
           <AnimatePresence>
             <motion.ul
@@ -50,20 +53,15 @@ export const Header = ({ path }: Props) => {
               exit="hidden"
             >
               {menuItems.map((menuItem) => (
-                <motion.li
-                  key={menuItem.title}
-                  variants={menuVariants}
-                  onClick={() => setIsOpen(false)}
-                >
+                <motion.li key={menuItem.title} variants={menuVariants} onClick={() => setIsOpen(false)}>
                   <MenuLink currPath={path} {...menuItem} />
                 </motion.li>
               ))}
-              <motion.li variants={menuVariants} className="mt-6">
-                <TicketButton />
-              </motion.li>
             </motion.ul>
           </AnimatePresence>
         )}
+
+        {/* Desktop Menu */}
         <ul className="flex-row items-center hidden space-x-8 xl:flex text-beige">
           {menuItems.map((menuItem) => (
             <motion.li key={menuItem.title} onClick={() => setIsOpen(false)}>
