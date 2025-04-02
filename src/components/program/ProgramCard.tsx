30e224821de338d1a325c1eecc0755a3824367e3
metalmasterboy <metalmasterboy@gmail.com>
2025-04-02 18:00:38 +0000

    Program for 2025

---
+++ src/components/program/ProgramCard.tsx
@@ -0,0 +1,112 @@
+import React, { useState, useRef, useEffect } from "react";
+
+interface ProgramProps {
+  time: string;
+  title: string;
+  description?: string;
+  expandedDescription?: string; // New prop for expanded description
+  bgColor: string; // Optional background color prop
+  imageSrc?: any; // Optional image URL
+}
+
+const ProgramCard: React.FC<ProgramProps> = ({ time, title, description, expandedDescription, bgColor, imageSrc }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(false); // New state to handle mobile expand/collapse
+  const cardRef = useRef<HTMLDivElement>(null); // Ref for the card
+
+  const toggleExpanded = () => {
+    setIsExpanded(!isExpanded); // Toggle expanded state on mobile
+  };
+
+  // Effect to handle clicks outside the card
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (cardRef.current && !cardRef.current.contains(event.target as Node)) {
+        setIsExpanded(false); // Close expanded description if clicked outside
+      }
+    };
+
+    // Attach the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+    
+    // Cleanup the event listener on component unmount
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [cardRef]);
+
+  return (
+    <div
+      ref={cardRef} // Attach the ref to the card
+      className={`relative bg-${bgColor} text-beige py-8 shadow-lg flex flex-col items-center w-full text-center`}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      onClick={toggleExpanded}
+    >
+      {/* Time positioned at the top quarter, full width */}
+      <div className="w-full absolute top-1/5">
+        <h2 className="text-3xl md:text-4xl font-bold text-center">{time}</h2>
+      </div>
+
+      {/* Content Section */}
+      <div className={`mt-16 flex items-center w-full px-6 pb-8 md:pb-0 ${imageSrc ? "text-left" : "text-center flex-col"}`}>
+        {imageSrc && <img src={imageSrc.src} alt={title} className="w-16 h-16 object-cover mr-4" />}
+        <div className={`flex-1 ${imageSrc ? "text-left" : "text-center"}`}>
+          <h3 className="text-base md:text-2xl font-semibold">{title}</h3>
+          <p className="text-base mt-1">{description}</p>
+        </div>
+      </div>
+
+      {/* Desktop Layout (Visible only on medium screens and above) */}
+      <div className="hidden md:block w-full">
+        {expandedDescription && (
+          <div
+            className={`absolute left-0 bottom-0 w-full min-h-48 text-left flex items-center bg-white text-dark-green p-6 shadow-2xl transition-all duration-300 ease-in-out ${
+              isHovered
+                ? "translate-y-full opacity-100 z-10"
+                : "translate-y-0 opacity-0 z-[-1]"
+            }`}
+          >
+            <div className="relative w-full flex items-center">
+              <p className="text-base whitespace-pre-line w-4/5">{expandedDescription}</p>
+            </div>
+            <div className={`absolute right-0 top-4 bottom-4 h-auto w-4 bg-${bgColor} z-50`} />
+          </div>
+        )}
+      </div>
+
+      {/* Mobile Layout (Visible only on small screens) */}
+      <div className="md:hidden w-full">
+        <div className="absolute bottom-4 right-4 md:hidden">
+          <button className="bg-transparent text-white px-4 py-2 rounded-md">
+            více → {/* Clicking this will trigger the expanded description */}
+          </button>
+        </div>
+
+        {/* Expanded Description for Mobile */}
+        {expandedDescription && (
+          <div
+            className={`absolute left-0 bottom-0 w-full min-h-48 text-left flex items-center bg-white text-dark-green p-6 pb-12 shadow-2xl transition-all duration-300 ease-in-out ${
+              isExpanded
+                ? "translate-x-0 opacity-100 z-10"
+                : "translate-x-full opacity-0 z-[-1]"
+            }`}
+            onClick={toggleExpanded}
+          >
+            <div className="relative w-full flex items-center">
+              <p className="text-base whitespace-pre-line w-4/5">{expandedDescription}</p>
+            </div>
+            <div className={`absolute right-0 top-4 bottom-4 h-auto w-4 bg-${bgColor} z-50`} />
+            <div className="absolute bottom-4 right-4 md:hidden">
+              <button className="bg-transparent text-dark-green px-4 py-2 rounded-md">
+                ← méně {/* Clicking this will collapse the expanded description */}
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ProgramCard;
--- src/sections/SectionProgram.tsx
+++ src/sections/SectionProgram.tsx
@@ -1,15 +1,16 @@
 import { ProgramIcon1, ProgramIcon2 } from '../components/icons/ProgramIcons';
 import { Section } from '../components/shared/Section';
+import ProgramList from '../components/program/ProgramList';
 
 export const SectionProgram = () => {
   return (
-    <Section id="program" title="Program" className="text-dark-green bg-beige pt-20">
+    <Section id="program" title="" className="text-dark-green bg-beige pt-20">
       <>
         {/*   width="173"
       height="270" */}
         <ProgramIcon1 className="absolute top-0 left-0" />
         <ProgramIcon2 className="absolute top-0 right-0 hidden md:block z-[1] w-[90px] h-[140px] lg:w-[173px] lg:h-[270px]" />
-        <div className="max-w-[700px] space-y-5 md:text-lg text-dark-green relative z-[2]">
+        {/* <div className="max-w-[700px] space-y-5 md:text-lg text-dark-green relative z-[2]">
           <p>
             Dvanáctý ročník, který se konal 3. dubna 2024 v kavárně 14|15 Baťova
             institutu ve Zlíně, nabídl exotickou atmosféru a zajímavé hosty. O
@@ -22,6 +23,16 @@ export const SectionProgram = () => {
           <p className="text-xl md:text-2xl font-semibold">
             Co přinese další ročník, se dozvíte brzy!
           </p>
+        </div> */}
+        <h2>Dopolední program (není pro veřejnost)</h2>
+        {/* Responsive Program Cards */}
+        <div className="relative px-0 md:px-12">
+          <ProgramList startId={1} endId={6} />
+        </div>
+        <h2 className="pt-4 md:pt-8">Odpolední program (je pro veřejnost)</h2>
+        {/* Responsive Program Cards */}
+        <div className="relative px-0 md:px-12">
+          <ProgramList startId={7} endId={15} />
         </div>
         {/* <p className="text-xl">Program moderuje Slávek Král</p> */}
         {/* <MainProgram /> */}
